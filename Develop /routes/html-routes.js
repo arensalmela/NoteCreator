c838b1a4012083485898973417acc3e3f41560ef
Aren S <adsalmela@gmail.com>
2020-12-11 19:32:30 -0500

    Updated server.js and html routes. Home page is displaying properly.

---
+++ Develop /routes/html-routes.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = function (app) {
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname + "./public/notes.html"));
+  });
+
+  app.use("*", function (req, res) {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+  });
+};
--- Develop /server.js
+++ Develop /server.js
@@ -1,23 +1,22 @@
 // Dependencies
 
 const express = require("express");
-
 const app = express();
+//const path = require("path");
 
 //Setting an initial port
 const PORT = 8080;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-//const htmlRoutes = require("./routes/htmlroutes.js");
-//const apiroutes = require("./routes/apiroutes.js");
+//app.get("/", function (req, res) {
+//res.send("hello world");
+//});
 
 //Setting up app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
